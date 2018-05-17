7d9bc8b53ce2408d9eb74f233bea4a2e91c24da5
mm13609 <mustafamohammad92@gmail.com>
2018-05-17 14:29:51 -0500

    Added a separate library for code first

--- CodeFirstTutorial/Program.cs
+++ CodeFirstTutorial/Program.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainData;
 
 namespace CodeFirstTutorial
 {
     class Program
     {
-        private static string continueProcess = "y";\
+        private static string continueProcess = "y";
+        private static ITrainRepository _trainRepository = new TrainRepository();
 
         static void Main(string[] args)
         {
             Console.WriteLine("These are the all the trains. Would like to add trains? (Y/N)");
 
-            foreach (var train in ListTrains())
+            foreach (var train in _trainRepository.ListTrains())
             {
                 Console.WriteLine($"Symbol: {train.TrainSymbol}, Speed: {train.Speed}, Description: {train.Description}");
             }
@@ -46,13 +48,6 @@ namespace CodeFirstTutorial
             }
         }
 
-        public static IEnumerable<Train> ListTrains()
-        {
-            using (var db = new TrainContext())
-            {
-                return db.Trains.AsNoTracking().ToList();
-            }
-        }
 
         private static void AddTrains()
         {
@@ -68,56 +63,11 @@ namespace CodeFirstTutorial
                 var trainStationName = Console.ReadLine();
                 Console.WriteLine("Please enter train station address");
                 var trainStationAddress = Console.ReadLine();
-                
-                Add(trainSymbol, Convert.ToInt32(speed), trainStationName, trainStationAddress);
+
+                _trainRepository.Add(trainSymbol, Convert.ToInt32(speed), trainStationName, trainStationAddress);
 
                 Console.WriteLine("Do you wish to continue ? Type (Y/N)");
                 continueProcess = Console.ReadLine();
-            }
-        }
-
-        public static void DeleteAllTrains()
-        {
-            using (var db = new TrainContext())
-            {
-                db.Trains.ToList().ForEach(x => db.Trains.Remove(x));
-                db.SaveChanges();
-            }
-        }
-
-        public static void Delete(int id)
-        {
-            using (var db = new TrainContext())
-            {
-                var trains = db.Trains.Where(x => x.TrainSymbol == "Mustafa Train");
-                db.Trains.RemoveRange(trains);
-                db.SaveChanges();
-            }
-        }
-
-        public static void Edit(string oldTrainSymbol, string trainSymbol, int speed = 0, string description = "")
-        {
-            using (var db = new TrainContext())
-            {
-               var train = db.Trains.FirstOrDefault(x => x.TrainSymbol == oldTrainSymbol);
-
-                if (train != null)
-                {
-                    train.TrainSymbol = trainSymbol;
-                    train.Speed = speed;
-                    train.Description = description;
-                    db.SaveChanges();
-                }
-            }
-        }
-
-        public static void Add(string trainSymbol, int speed, string stationName, string stationAddress, string description = "")
-        {
-            using (var db = new TrainContext())
-            {
-                db.Trains.Add(new Train { TrainSymbol = trainSymbol, Speed = speed, Description = description });
-                db.TrainStations.Add(new TrainStation { StationName = stationName, StationAddress = stationAddress });
-                db.SaveChanges();
             }
         }
     }
